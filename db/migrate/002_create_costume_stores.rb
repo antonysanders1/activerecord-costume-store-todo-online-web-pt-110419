506950ded89ac5e433901a4523f5a450e5df4d99
Antony Sanders <antony.devon92@gmail.com>
2020-02-16 01:04:00 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change 
+    create_table :costume_stores do |col|
+      col.string :name
+      col.string :location
+      col.integer :costume_inventory
+      col.integer :num_of_employees
+      col.boolean :still_in_business
+      col.datetime :start_time
+      col.datetime :end_time
+    end
+  end 
+end
